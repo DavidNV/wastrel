cd88dc4b5ecf7fdd1d3d9dafd3040514054a06fc
Orferus <david.nvalderrama@gmail.com>
2018-05-08 20:06:03 -0500

    Before API

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,14 +1,28 @@
 class ExpensesController < ApplicationController
   def index
-    transaction_type = params[:transaction_type]
-    category = params[:category]
-    puts "\n*" * 5
-    puts params.inspect
-    @expenses = Expense.first(10)
+    puts request.headers.to_h.keys
+    transaction_type = params["transaction_type"]
+    category = params["category"]
+    if transaction_type && !category
+      @expenses = Expense.joins(:transaction_type)
+                  .where("transaction_types.name = ?", transaction_type.capitalize).limit(10)
+
+    elsif category && !transaction_type
+      @expenses = Expense.joins(:category)
+                  .where("categories.name = ?", category.capitalize).limit(10)
+
+    elsif category && transaction_type
+      @expenses = Expense.joins(:transaction_type, :category)
+                   .where("transaction_types.name = ? AND categories.name = ?", transaction_type.capitalize, category.capitalize).limit(10)
+    else
+      @expenses = Expense.last(10)
+    end
     @transaction_types = TransactionType.all
+    @categories = Category.all
     respond_to do |format|
       format.html { render :index }
       format.json { render json: @expenses, status: :ok }
+      format.js { puts params.inspect; render partial: "expenses_list", locals: { cositas: @expenses } }
     end
   end
 end
