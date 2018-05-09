0267e2311a9d6d118f80fcda88d55f5a8c53ba24
Orferus <david.nvalderrama@gmail.com>
2018-05-08 20:39:31 -0500

    Update root2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :expenses, only: :index
 
-  root_to 'expenses#index'
+  root 'expenses#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
